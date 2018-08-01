00b15cba4504e79a6e633efef61226bafe6986f3
Nimila Hiranya <nimilas@hsenidmobile.com>
2018-08-01 18:21:42 +0530

    Prerelease commit for version 2.0.1

--- example/KenBurnsExample/index.android.js
+++ example/KenBurnsExample/index.android.js
@@ -3,7 +3,7 @@
  * Created by Nimila Hiranya Samarasinghe
  * Links: https://github.com/nHiRanZ
  *
- * Version: 1.0.0
+ * Version: 2.0.1
  *
  * Description: This reflects the KenBurns effect in a React Native Image
  */
